a8720898af0bfa1bf01a8b63e76ec4a65a665ab2
tomtrasmontero <tomtrasmontero@gmail.com>
2018-02-03 11:25:51 -0500

    added support for older browser, IE

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+// support older browser
+import 'babel-polyfill';
 
 import './index.css';
 import App from './App';
